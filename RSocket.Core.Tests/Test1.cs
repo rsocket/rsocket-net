d7ecf36527e0b21e41a678432780e83c97064379
dbarnett-netifi <dan@netifi.com>
2019-01-17 10:45:52 -0700

    Adding back trivial test to allow a test run to complete for CI.

---
+++ RSocket.Core.Tests/Test1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RSocket.Core.Tests
+{
+	[TestClass]
+	public class Test1
+	{
+		[TestInitialize]
+		public void TestInitialize()
+		{
+		}
+		[TestCleanup]
+		public void TestCleanup()
+		{
+		}
+
+		[TestMethod]
+		public void Test()
+		{
+		}
+	}
+}
