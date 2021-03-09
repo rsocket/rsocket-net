4eb27d000cd94e6a94a048b037ef1e0cbac3b884
shuxinqin <shuxinqin@outlook.com>
2021-03-09 16:24:48 +0800

    Add RSocketExtension.

---
+++ RSocket.Core/RSocketExtension.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSocket
+{
+	public static class RSocketExtension
+	{
+		public static Task<string> RequestResponse(this RSocket rsocket, string data, string metadata = default) => rsocket.RequestResponse(value => Encoding.UTF8.GetString(value.data.ToArray()), new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(data)), metadata == default ? default : new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(metadata)));
+		public static IAsyncEnumerable<string> RequestStream(this RSocket rsocket, string data, string metadata = default)
+		{
+			return rsocket.RequestStream(value =>
+			{
+				return Encoding.UTF8.GetString(value.data.ToArray());
+			}, new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(data)), metadata == default ? default : new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(metadata)));
+		}
+
+		public static IAsyncEnumerable<string> RequestChannel(this RSocket rsocket, IAsyncEnumerable<string> inputs, string data = default, string metadata = default) =>
+			rsocket.RequestChannel(inputs, input => new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(input)), result => Encoding.UTF8.GetString(result.data.ToArray()),
+				data == default ? default : new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(data)),
+				metadata == default ? default : new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(metadata)));
+	}
+}
